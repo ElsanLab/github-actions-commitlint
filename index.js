9146944a291856348c6d6dbcf6b13cf108c065aa
Clement Grihon <gclems@gmail.com>
2022-11-10 15:42:52 +0100

    0.0.16 - babel

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-import { getInput, setFailed } from "@actions/core";
-import { context as eventContext, getOctokit } from "@actions/github";
+'use strict';
 
-import load from "@commitlint/load";
-import lint from "@commitlint/lint";
+var core = require('@actions/core');
+var github = require('@actions/github');
+var load = require('@commitlint/load');
+var lint = require('@commitlint/lint');
 
-try {
-  console.log("Checking execution context...");
-  if (
-    process.env.GITHUB_REF_TYPE !== "branch" ||
-    process.env.GITHUB_EVENT_NAME !== "pull_request"
-  ) {
-    throw "THIS ACTION CAN ONLY BE TRIGGERED ON A PULL REQUEST";
-  }
-
-  const octokit = getOctokit(getInput("token"));
-  const { owner, repo, number } = eventContext.issue;
-  const { data: commits } = await octokit.rest.pulls.listCommits({
-    owner,
-    repo,
-    pull_number: number,
-  });
-
-  let messages = commits.map((c) => c.commit.message);
-  console.log("COMMIT MESSAGES", messages);
-
-  const config = await load({ extends: ["@commitlint/config-conventional"] });
-  console.log("config loaded", config);
-
-  const opts = {
-    parserOpts: config.parserPreset?.parserOpts ?? {},
-    plugins: config.plugins ?? {},
-    ignores: config.ignores ?? [],
-    defaultIgnores:
-      config.defaultIgnores != null ? config.defaultIgnores : true,
-  };
-
-  const results = [];
-  for (let message of messages) {
-    console.log("start linting", message);
-
-    const lintResult = await lint(message, config.rules, opts);
-
-    console.log("linted", lintResult);
-
-    results.push({
-      lintResult,
-      hash: commit.hash,
+async function action() {
+  try {
+    var _config$parserPreset;
+    console.log("Checking execution context...");
+    if (process.env.GITHUB_REF_TYPE !== "branch" || process.env.GITHUB_EVENT_NAME !== "pull_request") {
+      throw "THIS ACTION CAN ONLY BE TRIGGERED ON A PULL REQUEST";
+    }
+    const octokit = github.getOctokit(core.getInput("token"));
+    const {
+      owner,
+      repo,
+      number
+    } = github.context.issue;
+    const {
+      data: commits
+    } = await octokit.rest.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: number
     });
+    let messages = commits.map(c => c.commit.message);
+    console.log("COMMIT MESSAGES", messages);
+    const config = await load({
+      extends: ["@commitlint/config-conventional"]
+    });
+    console.log("config loaded", config);
+    const opts = {
+      parserOpts: ((_config$parserPreset = config.parserPreset) === null || _config$parserPreset === void 0 ? void 0 : _config$parserPreset.parserOpts) ?? {},
+      plugins: config.plugins ?? {},
+      ignores: config.ignores ?? [],
+      defaultIgnores: config.defaultIgnores != null ? config.defaultIgnores : true
+    };
+    const results = [];
+    for (let message of messages) {
+      console.log("start linting", message);
+      const lintResult = await lint(message, config.rules, opts);
+      console.log("linted", lintResult);
+      results.push({
+        lintResult,
+        hash: commit.hash
+      });
+    }
+    console.log("LINTED COMMITS", results);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  console.log("LINTED COMMITS", results);
-} catch (error) {
-  setFailed(error.message);
 }
+
+action();
